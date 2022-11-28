da3ec9d5a81049ee2b743d8dc2094403bb2b957d
techx1 <ss.uitech01@gmail.com>
2022-11-29 00:02:43 +0500

    test 1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 // import Products from '../components/Products/Products';
 
+import Products from '../components/Products/Products';
+
 export default function Home() {
   return (
     <div className='bg-[#040a47]'>
       <div id='products' className='page-content pt-16 z-1'>
-        <div className='products w-full mb-20'>{/* <Products /> */}</div>
+        <div className='products w-full mb-20'>
+          <Products />
+        </div>
       </div>
     </div>
   );
